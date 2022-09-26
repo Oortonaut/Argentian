39850cf9191ca3163738ef196af6fe7e24278c99
Ace Stapp <acy.stapp@gmail.com>
2022-09-25 23:01:52 -0400

    Added findRoot argument to initialize and enhanced command line parsing. The following argument types are allowed:
    [[ arg => result
    --foo bar;baz => bar;baz
    --foo:bar => bar (colon is ignored)
    --foo+ => +
    --foo/bar => /bar

--- Core/Config.cs
+++ Core/Config.cs
@@ -10,38 +10,42 @@ namespace Argentian.Core {
         public static List<string> dataPath = new List<string>();
         public static List<string> shaderPath = new List<string>();
         public static List<string> texturePath = new List<string>();
-        public static string[] args = new string[]{ };
-
-        public static bool GetFlag(string tag, bool d = false) {
-            foreach (var arg in args) {
-                if (arg.StartsWith(tag)) {
-                    var opt = arg.Substring(tag.Length);
-                    if (opt == "-")
-                        return false;
-                    else if (opt == "+")
-                        return true;
-                    else if (opt == "")
-                        return true;
-                    else
-                        throw new InvalidDataException($"Unrecognized flag {arg}");
-                }
-            }
-            return d;
-        }
-        public static string GetString(string tag, string d = "") {
+        public static string[] args = new string[] { };
+        //     /====/ /======/ /==/   /===============/
+        // --foo bar;baz    => bar;baz
+        // --foo:bar        => bar (colon is ignored)
+        // --foo+           => +
+        // --foo/bar        => /bar
+       public static string GetString(string tag, string d = "") {
             int index = 0;
             foreach (var arg in args) {
-                if (arg.StartsWith(tag)) {
-                    var opt= arg.Substring(tag.Length);
-                    if (opt.Length == 0) {
+                if (arg == tag) {
+                    if (index + 1 < args.Length) {
                         return args[index + 1];
+                    }
+                } else if (arg.StartsWith(tag)) {
+                    if (tag[0] == ':') {
+                        return arg.Substring(tag.Length + 1);
                     } else {
-                        return opt;
+                        return arg.Substring(tag.Length);
                     }
                 }
                 ++index;
             }
             return d;
+        }
+        public static bool GetFlag(string tag, bool d = false) {
+            var s = GetString(tag, d.ToString());
+            return s.Length == 0 || !("NnFf-".Contains(s[0]));
+        }
+        public static int GetInt(string tag, int d = 0) {
+            var s = GetString(tag, d.ToString());
+            return Convert.ToInt32(s);
+        }
+        public static (int x, int y) GetInt2(string tag, int x = 0, int y = 0) {
+            var s = GetString(tag, $"{x},{y}");
+            var ss = s.Split(',');
+            return (Convert.ToInt32(ss[0]), Convert.ToInt32(ss[1]));
         }
         public static string FindRoot() {
             var current = Fwd(Directory.GetCurrentDirectory());
@@ -49,24 +53,25 @@ namespace Argentian.Core {
             parts.RemoveAt(parts.Count - 1);
             while(parts.Any()) {
                 var path = Path.Combine(parts.ToArray());
-                if(Directory.Exists($"{path}/shaders") &&
-                    Directory.Exists($"{path}/textures")) {
+                if(Directory.Exists($"{path}/shaders")) {
                     return path;
                 }
                 parts.RemoveAt(parts.Count - 1);
             }
             return current;
         }
-        public static void Initialize(string[] args_) {
+        public static void Initialize(bool findRoot, string[] args_) {
             args = args_;
-            string rootPath = GetString("--root", "./");
-
+            string rootPath = findRoot ? FindRoot() : GetString("--root", "./");
             foreach (var entry in rootPath.Split(new[] { ';' })) {
                 var normalized = Path.GetFullPath(entry);
                 normalized = Fwd(normalized);
                 shaderPath.AddRange(Glob.Directories(normalized, "**/shaders").Select(x => normalized + Fwd(x)));
                 texturePath.AddRange(Glob.Directories(normalized, "**/textures").Select(x => normalized + Fwd(x)));
             }
+        }
+        public static void Initialize(string[] args_) {
+            Initialize(true, args);
         }
         static string Fwd(string x, bool path = true) {
             x = x.Replace('\\', '/');
